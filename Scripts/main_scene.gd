5e32cbe492ce1ccd77c6fb223ddfee0c7cc51e68
star6fish <jjansen@students.mags.school.nz>
2025-07-16 09:45:54 +1200

    15/07/2025

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -35,9 +35,12 @@ func _load_building_catalogue():
 		
 # Selects the building that the player has pressed
 func _select_building(building_name):
-	current_building = get_node(global.buildings[building_name].scene).instantiate()
+	
+	current_building = global.buildings[building_name].scene.instantiate()
 	
 	add_child(current_building)
+	
+	current_building.position = Vector3(-0.188, 0, -3.622)
 	
 	for i : Control in get_node("Control/ScrollContainer/HBoxContainer").get_children():
 		if i.get_meta("building") == building_name:
@@ -52,4 +55,18 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	if current_building != null:
+		var mouse_position = get_viewport().get_mouse_position()
+		
+		var space_state = get_world_3d().direct_space_state
+		
+		var origin = $Camera3D.project_ray_origin(mouse_position)
+		var direction = $Camera3D.project_ray_normal(mouse_position)
+		
+		var query = PhysicsRayQueryParameters3D.create(origin, origin + direction * 20000)
+		query.collide_with_areas = true
+		var mouse_position3D = space_state.intersect_ray(query)
+		print(mouse_position3D)
+		
+		#current_building.position = mouse_position3D
